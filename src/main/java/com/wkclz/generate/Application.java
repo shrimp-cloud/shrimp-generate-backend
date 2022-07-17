cb9c39763a88ef8ceb655670193ddf248f4e66bd
wangkc <wkclz@qq.com>
2022-07-17 22:26:29 +0800

    还是得扫描。。

--- src/main/java/com/wkclz/generate/Application.java
+++ src/main/java/com/wkclz/generate/Application.java
@@ -2,8 +2,10 @@ package com.wkclz.generate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = {"com.wkclz"})
 public class Application {
 
     public static void main(String[] args) {
